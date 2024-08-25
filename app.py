f075e6727d048b900f59efe2174f57c7dadef609
Shivakant Dubey <shivakant395@gmail.com>
2024-08-25 12:09:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 def process_data(data):
     # Send a POST request to the backend API
-    url = "http://localhost:8000/"  # Replace with your backend API URL
+    url = "http://localhost:8000"  # Replace with your backend API URL
     headers = {"Content-Type": "application/json"}
     response = requests.post(url, headers=headers, data=json.dumps(data))
 
@@ -16,28 +16,11 @@ def process_data(data):
     else:
         return {"error": "Failed to process the data"}
 
-def get_operation_code():
-    # Send a GET request to the backend API
-    url = "http://localhost:8000/bfhl"  # Replace with your backend API URL
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Extract the response data
-        response_data = response.json()
-        return response_data
-    else:
-        return {"error": "Failed to get operation code"}
-
 def main():
     st.title("Data Processing App")
 
-    # Get operation code
-    operation_code = get_operation_code()
-    st.write("Operation Code:", operation_code)
-
     # Get user input
-    input_data = st.text_area("Enter JSON data", placeholder='{"user_id": "123", "college_email": "example@college.edu", "college_roll_number": "2020ABC123", "numbers": [1, 2, 3], "alphabets": ["A", "B", "c"]}')
+    input_data = st.text_area("Enter JSON data", placeholder='{"data": ["A", "C", "z"]}')
 
     if st.button("Process Data"):
         # Parse the input JSON
