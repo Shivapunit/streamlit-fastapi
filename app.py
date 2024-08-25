7f403ff2be14d9ccdb21fbe9672d1f7602070579
Shivakant Dubey <shivakant395@gmail.com>
2024-08-25 12:32:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def main():
     )
 
     # Header
-    st.header("Data Processing Application")
+    st.header("Bajaj Finserv Health Challenge: By Aastha Tiwari")
 
     # Big Title
     st.markdown("<h1 style='text-align: center; color: red;'>21BCE5542</h1>", unsafe_allow_html=True)
